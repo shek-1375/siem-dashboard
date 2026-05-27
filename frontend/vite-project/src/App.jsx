9167c93599fb12cec66b62894bccb55630e1d6b8
abhi1375 <abhishekramesh1375@gmail.com>
2026-05-27 22:42:55 +0530

    Initial SIEM dashboard setup

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+function App() {
+
+  const [message, setMessage] = useState("Loading...")
+
+  useEffect(() => {
+
+    axios.get("http://127.0.0.1:5000/")
+      .then((response) => {
+        setMessage(response.data.message)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+  }, [])
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  )
+}
+
+export default App
